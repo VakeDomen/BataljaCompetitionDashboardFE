4300e25bc1af4dfc6ef1f776bec147d8bc8d20df
VakeDomen <vake.domen94@gmail.com>
2023-11-13 19:29:15 +0000

    basic game rendering

--- src/app/components/game-canvas/game-canvas.component.ts
+++ src/app/components/game-canvas/game-canvas.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
@@ -7,10 +8,27 @@ import { Component, AfterViewInit } from '@angular/core';
 })
 export class GameCanvasComponent implements AfterViewInit {
 
+  constructor(
+    private http: HttpClient,  
+  ) { }
+
   ngAfterViewInit(): void {
     // Assuming the init function is available in the global scope
     // because of the gameLogic.js being included in the scripts array of angular.json
-    (window as any).init();
+    this.loadLogFile()
   }
 
+  loadLogFile() {
+    // Adjust the path based on the location of your log.txt file in the assets folder
+    const logFilePath = 'assets/log.txt';
+
+    this.http.get(logFilePath, { responseType: 'text' }).subscribe(
+      (data) => {
+        (window as any).initializeGame("canvas", data, "file");
+      },
+      (error) => {
+        console.error('Error loading log file:', error);
+      }
+    );
+  }
 }
