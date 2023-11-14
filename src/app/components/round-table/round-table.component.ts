87eb9e95e2000176faeab9723013b96dc6edfd96
VakeDomen <vake.domen94@gmail.com>
2023-11-14 11:06:45 +0000

    improved game table

--- src/app/components/round-table/round-table.component.ts
+++ src/app/components/round-table/round-table.component.ts
@@ -3,7 +3,7 @@ import { Bot } from 'src/app/models/bot.model';
 import { Game2v2 } from 'src/app/models/game.model';
 import { Team } from 'src/app/models/team.model';
 
-type BotColor = 'red' | 'yellow' | 'blue' | 'green';
+type BotSelector = 'my1' | 'my2' | 'e1' | 'e2';
 
 @Component({
   selector: 'app-round-table',
@@ -20,15 +20,15 @@ export class RoundTableComponent implements OnChanges {
 
   }
 
-  public getBotLabel(game: Game2v2, bot: BotColor): string {
-    if (this.isMyBot(game, bot)) {
+  public getBotLabel(game: Game2v2, bot: BotSelector): string {
+    if (bot == 'my1' || bot == 'my2') {
       return this.getBotName(game, bot);
     } else {
       return "Enemy"
     }
   }
 
-  private getBotName(game: Game2v2, bot: BotColor): string {
+  private getBotName(game: Game2v2, bot: BotSelector): string {
     const botId = this.getBotId(game, bot);
     if (!botId) {
       return "Unknown bot"
@@ -37,37 +37,48 @@ export class RoundTableComponent implements OnChanges {
     return this.bots.filter((b: Bot) => b.id == botId)[0].bot_name;
   }
 
-  public botSurvived(game: Game2v2, bot: BotColor): boolean {
-    if (bot == 'red') {
-      return game.team1bot1_survived;
+  public botSurvived(game: Game2v2, bot: BotSelector): boolean {
+    if (bot == 'my1') {
+      if (this.isTeam1(game)) return game.team1bot1_survived;
+      else return game.team2bot1_survived;
     }
-    if (bot == 'green') {
-      return game.team1bot2_survived;
+    if (bot == 'my2') {
+      if (this.isTeam1(game)) return game.team1bot2_survived;
+      else return game.team2bot2_survived;
     }
-    if (bot == 'blue') {
-      return game.team2bot1_survived;
+    if (bot == 'e1') {
+      if (this.isTeam1(game)) return game.team2bot1_survived;
+      else return game.team1bot1_survived;
     }
-    if (bot == 'yellow') {
-      return game.team2bot2_survived;
+    if (bot == 'e2') {
+      if (this.isTeam1(game)) return game.team2bot2_survived;
+      else return game.team1bot2_survived;
     }
     return false;
   }
 
-  private getBotId(game: Game2v2, bot: BotColor): string | undefined {
-    let botId
-    if (bot == 'red') {
-      botId = game.team1bot1_id;
+  private isTeam1(game: Game2v2): boolean {
+    return game.team1_id == this.team?.id
+  }
+
+  private getBotId(game: Game2v2, bot: BotSelector): string | undefined {
+    if (bot == 'my1') {
+      if (this.isTeam1(game)) return game.team1bot1_id;
+      else return game.team2bot1_id;
     }
-    if (bot == 'green') {
-      botId = game.team1bot2_id;
+    if (bot == 'my2') {
+      if (this.isTeam1(game)) return game.team1bot2_id;
+      else return game.team2bot2_id;
     }
-    if (bot == 'blue') {
-      botId = game.team2bot1_id;
+    if (bot == 'e1') {
+      if (this.isTeam1(game)) return game.team2bot1_id;
+      else return game.team1bot1_id;
     }
-    if (bot == 'yellow') {
-      botId = game.team2bot2_id;
+    if (bot == 'e2') {
+      if (this.isTeam1(game)) return game.team2bot2_id;
+      else return game.team1bot2_id;
     }
-    return botId;
+    return undefined;
   }
 
   public getEloLabel(game: Game2v2): string {
@@ -78,29 +89,8 @@ export class RoundTableComponent implements OnChanges {
     }
   }
 
-  public isMyBot(game: Game2v2, bot: BotColor): boolean {
-    let botTeam = ""
-    if (bot == 'red') {
-      botTeam = game.team1_id;
-    }
-    if (bot == 'green') {
-      botTeam = game.team1_id;
-    }
-    if (bot == 'blue') {
-      botTeam = game.team2_id;
-    }
-    if (bot == 'yellow') {
-      botTeam = game.team2_id;
-    }
-    return botTeam == this.team?.id;
-  }
-
-  public isGameWonByBot(game: Game2v2, bot: BotColor): boolean {
-    if (bot == 'blue' || bot == 'yellow') {
-      return game.winner_id == game.team2_id;
-    } else {
-      return game.winner_id == game.team1_id;
-    }
+  public isGameWonByBot(game: Game2v2, bot: BotSelector): boolean {
+    return game.winner_id == this.team?.id;
   }
 
   public isGameWon(game: Game2v2): boolean {
